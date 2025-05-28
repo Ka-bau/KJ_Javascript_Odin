34165c72259cd4012dba6145a51dd56a05e9081e
ka-bau <kabaujohn@gmail.com>
2025-05-28 12:54:14 +0300

    chore: final submission of Project Rock Paper Scissors after debugging.

--- Project-RPS/rps.js
+++ Project-RPS/rps.js
@@ -1,9 +1,6 @@
-let humanScore = 0;
-let computerScore = 0;
-
 function getHumanChoice (){
     let humanChoice = prompt('Please enter your choice: ');
-    return humanChoice.toLowerCase;
+    return humanChoice.toLowerCase();
 }
 
 function getComputerChoice (){
@@ -14,51 +11,56 @@ function getComputerChoice (){
     return computerChoice;
 }
 
-function playRound (humanSelection,computerSelection){
-    
-    let humanSelection = getHumanChoice();
-    let computerSelection = getComputerChoice();
-
-
-    if (humanSelection === computerSelection){
-        return 'You draw! No points for each.';
-
-    }else if ((humanSelection === 'rock') && (computerSelection === 'paper')){
-        computerScore++;
-        return 'You lose! Paper beats rock!';
-        
-    }else if ((humanSelection === 'rock') && (computerSelection === 'scissors')){
-        humanScore++;
-        return 'You win! Rock beats scissors';
-        
-    }else if ((humanSelection === 'paper') && (computerSelection === 'scissors')){
-        computerScore++;
-        return 'You lose! Scissors beats paper!';
-        
-    }else if ((humanSelection === 'paper') && (computerSelection === 'rock')){
-        humanScore++;
-        return 'You win! Paper beats rock!';
-        
-    }else if ((humanSelection === 'scissors') && (computerSelection === 'rock')){
-        computerScore++;
-        return 'You lose! Rock beats scissors!';
-        
-    }else if ((humanSelection === 'scissors') && (computerSelection === 'paper')){
-        humanScore++;
-        return 'You win! Scissors beats Paper!';
-        }
-}
-
 function playGame(){
-    let playedRound = 0
+    let humanScore = 0;
+    let computerScore = 0;
+    let playedRound = 0;
 
-    while (playedRound = 0, playedRound < 5, playedRound++){
-        console.log(playRound);
-    }if (humanScore > computerScore){
-    return 'You win with a score of' +''+humanScore;
-   }else if (computerScore > humanScore){
-    return 'You lose. Computer wins with a score of'+''+computerScore;
-   }else{
-    return 'No winner, both tie';
-   }
+        function playRound (){
+    
+            let humanSelection = getHumanChoice();
+            let computerSelection = getComputerChoice();
+
+            if (humanSelection === computerSelection){
+                computerScore +=0;
+                humanScore +=0;
+            return 'You draw! No points for each.';
+
+            }else if ((humanSelection === 'rock') && (computerSelection === 'paper')){
+                computerScore++;
+                return 'You lose! Paper beats rock!';
+        
+            }else if ((humanSelection === 'rock') && (computerSelection === 'scissors')){
+                humanScore++;
+                return 'You win! Rock beats scissors';
+        
+            }else if ((humanSelection === 'paper') && (computerSelection === 'scissors')){
+                computerScore++;
+                return 'You lose! Scissors beats paper!';
+        
+            }else if ((humanSelection === 'paper') && (computerSelection === 'rock')){
+                humanScore++;
+                return 'You win! Paper beats rock!';
+        
+            }else if ((humanSelection === 'scissors') && (computerSelection === 'rock')){
+                computerScore++;
+                return 'You lose! Rock beats scissors!';
+        
+            }else if ((humanSelection === 'scissors') && (computerSelection === 'paper')){
+                humanScore++;
+                return 'You win! Scissors beats Paper!';
+            }
+        }
+
+    while (playedRound < 5){
+        console.log(playRound());
+        playedRound++;
+
+        }if (humanScore > computerScore){
+            return 'You win with a score of' +''+humanScore;
+        }else if (computerScore > humanScore){
+            return 'You lose. Computer wins with a score of'+''+computerScore;
+        }else{
+            return 'No winner, both tie';
+    }
 }
