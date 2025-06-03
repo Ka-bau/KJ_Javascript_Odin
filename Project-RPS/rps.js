3940abd76698d35c251b42bbf80933028647b020
ka-bau <kabaujohn@gmail.com>
2025-06-03 16:56:27 +0300

    chore:debugged the code by adding reset of scores in the playGame funtion.

--- Project-RPS/rps.js
+++ Project-RPS/rps.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getHumanChoice (){
     let humanChoice = prompt('Please enter your choice: ');
     return humanChoice.toLowerCase();
@@ -11,56 +14,57 @@ function getComputerChoice (){
     return computerChoice;
 }
 
-function playGame(){
-    let humanScore = 0;
-    let computerScore = 0;
+function playRound (){
+
+    let humanSelection = getHumanChoice();
+    let computerSelection = getComputerChoice();
+
+    if (humanSelection === computerSelection){
+    return 'You draw! No points for each.';
+
+    }else if ((humanSelection === 'rock') && (computerSelection === 'paper')){
+        computerScore++;
+        return 'You lose! Paper covers rock!';
+
+    }else if ((humanSelection === 'rock') && (computerSelection === 'scissors')){
+        humanScore++;
+        return 'You win! Rock crushes scissors';
+
+    }else if ((humanSelection === 'paper') && (computerSelection === 'scissors')){
+        computerScore++;
+        return 'You lose! Scissors cut paper!';
+
+    }else if ((humanSelection === 'paper') && (computerSelection === 'rock')){
+        humanScore++;
+        return 'You win! Paper covers rock!';
+
+    }else if ((humanSelection === 'scissors') && (computerSelection === 'rock')){
+        computerScore++;
+        return 'You lose! Rock crushes scissors!';
+
+    }else if ((humanSelection === 'scissors') && (computerSelection === 'paper')){
+        humanScore++;
+        return 'You win! Scissors cut Paper!';
+    }
+}
+
+function playGame(playRound){
+
     let playedRound = 0;
+    humanScore = 0;
+    computerScore = 0;
 
-        function playRound (){
-    
-            let humanSelection = getHumanChoice();
-            let computerSelection = getComputerChoice();
-
-            if (humanSelection === computerSelection){
-                computerScore +=0;
-                humanScore +=0;
-            return 'You draw! No points for each.';
-
-            }else if ((humanSelection === 'rock') && (computerSelection === 'paper')){
-                computerScore++;
-                return 'You lose! Paper beats rock!';
-        
-            }else if ((humanSelection === 'rock') && (computerSelection === 'scissors')){
-                humanScore++;
-                return 'You win! Rock beats scissors';
-        
-            }else if ((humanSelection === 'paper') && (computerSelection === 'scissors')){
-                computerScore++;
-                return 'You lose! Scissors beats paper!';
-        
-            }else if ((humanSelection === 'paper') && (computerSelection === 'rock')){
-                humanScore++;
-                return 'You win! Paper beats rock!';
-        
-            }else if ((humanSelection === 'scissors') && (computerSelection === 'rock')){
-                computerScore++;
-                return 'You lose! Rock beats scissors!';
-        
-            }else if ((humanSelection === 'scissors') && (computerSelection === 'paper')){
-                humanScore++;
-                return 'You win! Scissors beats Paper!';
-            }
-        }
 
     while (playedRound < 5){
         console.log(playRound());
         playedRound++;
 
         }if (humanScore > computerScore){
-            return 'You win with a score of' +''+humanScore;
+            return 'You win with a score of' +' '+humanScore;
         }else if (computerScore > humanScore){
-            return 'You lose. Computer wins with a score of'+''+computerScore;
+            return 'You lose. Computer wins with a score of'+' '+computerScore;
         }else{
             return 'No winner, both tie';
     }
 }
+playGame();
